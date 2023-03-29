c05c6e1ce1df871ab84efd8152c42dc6eddaaf67
Federico Babbini <federico.babbini@avanade.com>
2023-03-29 17:36:21 +0200

    Starting an MVC project

---
+++ Web.Library/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Proxy.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Protocols;
+
+namespace Web.Library.Controllers
+{
+    public class BookController : Controller
+    {
+        // 
+        // GET: /Book/ 
+
+        public ActionResult Index()
+        {
+            var apiBook = new API_BookProxy();
+            var listBooks = apiBook.ReadBooks();
+            ViewData["BookList"] = listBooks;
+            return View();
+           
+        }
+
+    
+    }
+}
